57f8833231fae1916254a82e8a31e1fd44424682
Heroji Rao <128739593+rheroji@users.noreply.github.com>
2023-06-14 16:58:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// const wrapper = document.querySelector('.wrapper');
+// const loginLink = document.querySelector('.login-link');
+// const registerLink = document.querySelector('.register-link');
+
+
+
+
+// loginLink.addEventListener('click',()=> {
+//     wrapper.classList.removr('active');
+// });
